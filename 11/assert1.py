ff4f216ac9fba451cc9898adec27a0f33b384bba
bazi <743604512@qq.com>
2019-12-05 09:36:13 +0800

    文件和异常

---
+++ 11/assert1.py
@@ -0,0 +1,4 @@
+import sys
+assert ('linux' in sys.platform), "改代码只能在 Linux 下执行"
+# print(sys.platform)
+# assert ('win32' in sys.platform), "改代码只能在 win32 下执行"
---
+++ 11/json1.py
@@ -0,0 +1,31 @@
+import json
+
+
+def main():
+    mydict = {
+        'name': '骆昊',
+        'age': 38,
+        'qq': 957658,
+        'friends': ['王大锤', '白元芳'],
+        'cars': [
+            {'brand': 'BYD', 'max_speed': 180},
+            {'brand': 'Audi', 'max_speed': 280},
+            {'brand': 'Benz', 'max_speed': 320}
+        ]
+    }
+    try:
+        with open('data.json','w',encoding='utf-8') as fs:
+            json.dump(mydict,fs)
+            print(json.dumps(mydict))
+    except IOError as e:
+        print(e)
+    print('保存数据完成')
+
+
+def loadfile():
+    fs = open('data.json','r',encoding='utf-8')
+    print(json.load(fs))
+
+if __name__ == '__main__':
+    # main()
+    loadfile()
